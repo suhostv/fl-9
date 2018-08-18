a2a8606f3733941bfa19d8f0e8ce6d67c0615f4e
suhostv <suhost.v@gmail.com>
2018-08-18 15:29:10 +0300

    task done

--- FE_9_7_homework_control-flow/homework/src/js/task1.js
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -1,1 +1,23 @@
-// Your code goes here
+(function verification () {
+    let login = prompt('Please, input your username', '');
+  
+    if (login === '' || login === null) {
+        alert('Canceled');
+    } else if (login.length < 4) {
+        alert('I don’t know any users having name length less then 4 symbols');
+    } else if (login !== 'User') {
+        alert('I don’t know you');
+    } else {
+        let password = prompt('Please, enter your password', '');
+      
+        if (password === '' || password === null) {
+            alert('Canceled');
+        } else if (password !== 'SuperUser') {
+            alert('Wrong password');
+        } else if (new Date().getHours() < 20) {
+            alert('Good day!');
+        } else {
+            alert('Good evening!');
+        }
+    }
+}());
--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -1,1 +1,77 @@
-// Your code goes here
+(function guessingGame() {
+    let start = confirm('Do you want to play a game?');
+    let gameInfo = {
+        rangeStart : 0,
+        rangeEnd : 5,
+        rangeMultiplier : 6
+    }
+
+    function onCancel() {
+        alert('You did not become a millionaire, but can.');
+    }
+
+    function endOfGameConfirmation(callback, prizeWin) {
+        alert('Thank you for your game. Your prize is: ' + prizeWin);
+        confirm('Want to play again?') ? callback() : alert('See you next time');
+    }
+
+    function generateNumber (maxRange) {
+        return Math.floor(Math.random() * ++maxRange);
+    }
+    
+    (function game () {
+        let prize = 10; 
+        let totalPrize = 0;
+        let currentRange = gameInfo.rangeEnd;
+        
+        if (start === false) {
+            onCancel();
+        } else {                    
+            (function round() {
+            let myNumber = generateNumber(currentRange);
+            console.log(myNumber);
+            
+                for (let i = 0, currentPrize = prize; i < 3; i++) {
+                    let userNumber = prompt('Enter a number from ' + gameInfo.rangeStart + 
+                    ' to ' + currentRange + '\n' +
+                    'Attempts left: ' + (3-i) + '\n' +
+                    'Total prize: ' + totalPrize + '$\n' +
+                    'Possible prize on current attempt: ' + Math.floor(currentPrize) +'$' 
+                    ,'');
+                    
+                    
+
+                    if (userNumber === null) {
+                        onCancel();
+                        break;
+                    }
+                   
+                    userNumber = +userNumber;
+                    
+                    if (myNumber === userNumber) {
+                        totalPrize += Math.floor(currentPrize);
+                        let playAgain = confirm('Congratulations! Your prize is: ' + 
+                        totalPrize + '$\nDo you want to contunue?');
+                        
+                        if (!playAgain) {
+                            endOfGameConfirmation(game, totalPrize);
+                            break;
+                        } else {
+                            currentRange *= 2;
+                           
+                            prize *= 3; 
+                            currentPrize = prize;
+                            round();
+                            break;
+                        }
+                    } else if (i === 3) {
+                        endOfGameConfirmation(game, totalPrize);
+                        break;
+                    } else {
+                        currentPrize /= 2;
+                    }
+                }
+            }());    
+        }
+    }());    
+}());
