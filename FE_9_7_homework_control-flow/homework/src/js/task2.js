a9f0b0d7b2c39397c975937a60b9107dffe464d2
suhostv <suhost.v@gmail.com>
2018-08-21 20:21:59 +0300

    7th: moved to template string, 8th: starting commit

--- FE_9_7_homework_control-flow/homework/src/js/task2.js
+++ FE_9_7_homework_control-flow/homework/src/js/task2.js
@@ -7,6 +7,11 @@
         prizeMultiplier : 3,
         rangeMultiplier : 2
     }
+    let inputTemplate = (minRange, maxRange, attempts, total, current) => `
+    Enter a number from ${minRange} to ${maxRange}
+    Attempts left: ${attempts}
+    Total prize: ${total}
+    Possible prize on current attempt: ${Math.floor(current)}$`;
     let wannaPlay = true;
     let wannaContinue;
 
@@ -22,25 +27,22 @@
                 let myNumber = generateNumber(range);
                 
                 for (let i = 0, currentPrize = prize; i < 3; i++) {
-                    let userNumber = +prompt('Enter a number from ' + gameInfo.rangeStart + 
-                    ' to ' + range + '\n' +
-                    'Attempts left: ' + (3-i) + '\n' +
-                    'Total prize: ' + totalPrize + '$\n' +
-                    'Possible prize on current attempt: ' + Math.floor(currentPrize) +'$' 
-                    ,''); 
+                    let userNumber = +prompt(inputTemplate(gameInfo.rangeStart, range, (3-i), totalPrize, currentPrize)
+                    , ''); 
                     
                     if (myNumber === userNumber) {
                         totalPrize += Math.floor(currentPrize);
                         
-                        let playAgain = confirm('Congratulations! Your prize is: ' + 
-                            totalPrize + '$\nDo you want to continue?')
-                        if (!playAgain) {
-                            endOfGameConfirmation(totalPrize);
-                            break;
-                        } else {
+                        if ( confirm(`
+Congratulations! Your prize is: ${totalPrize} $. 
+Do you want to continue?`)) {
                             range *= gameInfo.rangeMultiplier;                           
                             prize *= gameInfo.prizeMultiplier; 
                             currentPrize = prize;
+                            break;
+                            
+                        } else {
+                            endOfGameConfirmation(totalPrize);
                             break;
                         }
                     } else if (i === 2) {
@@ -53,13 +55,13 @@
             }
         }
     } else {
-        alert('You did not become a millionaire, but can.');
+        alert(`You did not become a millionaire, but can.`);
     }
 
     function endOfGameConfirmation(prizeWin) {
         wannaContinue = false;
-        alert('Thank you for your game. Your prize is: ' + prizeWin);
-        if (!confirm('Want to play again?')) {
+        alert(`Thank you for your game. Your prize is: ${prizeWin}`);
+        if (!confirm(`Want to play again?`)) {
             wannaPlay = false;
             return wannaPlay; 
         }
